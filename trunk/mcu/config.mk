f3e2264a67a95dd1612e169791105b0342769839
murillo128 <murillo128@9f3d68e9-f188-4c09-b373-cf7d6a7ae904>
2013-07-16 13:27:10 +0000

    Static compilation and minor fixes

--- trunk/mcu/config.mk
+++ trunk/mcu/config.mk
@@ -3,6 +3,7 @@
 ##################################
 LOG		= yes
 DEBUG 		= yes
+STATIC		= no
 VADWEBRTC	= no
 WEBRTCINCLUDE	= /usr/local/src/webrtc/trunk/
 WEBRTDIROBJ	= /usr/local/src/webrtc/trunk/out/Release/obj.target/webrtc
