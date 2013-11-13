f65b74b84f33c1780c6dda674c77b6929e177b5b
murillo128 <murillo128@9f3d68e9-f188-4c09-b373-cf7d6a7ae904>
2013-11-13 16:54:47 +0000

    Added missing files

---
+++ trunk/mcu/include/websockets.h
@@ -0,0 +1,34 @@
+/* 
+ * File:   websockets.h
+ * Author: Sergio
+ *
+ * Created on 3 de enero de 2013, 12:40
+ */
+
+#ifndef WEBSOCKETS_H
+#define	WEBSOCKETS_H
+
+class WebSocket
+{
+public:
+	enum MessageType { Text = 0, Binary = 1 };
+	class Listener
+	{
+	public:
+		virtual void onOpen(WebSocket *ws) = 0;
+		virtual void onMessageStart(WebSocket *ws,const WebSocket::MessageType type) = 0;
+		virtual void onMessageData(WebSocket *ws,const BYTE* data, const DWORD size) = 0;
+		virtual void onMessageEnd(WebSocket *ws) = 0;
+		virtual void onError(WebSocket *ws) = 0;
+		virtual void onClose(WebSocket *ws) = 0;
+	};
+public:
+	virtual void Accept(Listener *listener) = 0;
+	virtual void Reject(const WORD code, const char* reason) = 0;
+	virtual void SendMessage(const std::string& message) = 0;
+	virtual void SendMessage(const BYTE* data, const DWORD size) = 0;
+	virtual void Close() = 0;
+};
+
+#endif	/* WEBSOCKETS_H */
+
