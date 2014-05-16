4e33eaf26ca710b614015c297e60ea01550ee084
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2014-05-16 10:32:33 +0000

    Fixed typo

--- src/main.cpp
+++ src/main.cpp
@@ -363,6 +363,15 @@ int main(int argc,char **argv)
 	//Start cpu monitor
 	monitor.Start(10000);
 
+	//Create converence
+	MultiConf* conf;
+	int confId = mcu.CreateConference(L"vnc",0);
+	mcu.GetConferenceRef(confId,&conf);
+	conf->Init(0,8000);
+	conf->AppMixerDisplayImage("/screen.png");
+	mcu.ReleaseConferenceRef(confId);
+
+
 	//Run it
 	server.Start();
 
