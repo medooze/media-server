a568da9ed951448d86596183ab64f1bb2d51a95b
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2018-11-14 11:26:04 +0100

    Add dumper interface

---
+++ include/UDPDumper.h
@@ -0,0 +1,13 @@
+#ifndef UDPDUMPER_H
+#define UDPDUMPER_H
+
+class UDPDumper
+{
+public:
+	virtual ~UDPDumper() = default;
+	virtual void WriteUDP(uint64_t currentTimeMillis,uint32_t originIp, short originPort, uint32_t destIp, short destPort,uint8_t* data, uint32_t size) = 0;
+	virtual void Close() = 0;
+};
+
+#endif /* UDPDUMPER_H */
+
