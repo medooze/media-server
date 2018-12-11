8a3070422bf6162b410104423ca960632770e13e
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2018-12-11 12:05:44 +0100

    [SLOW-FUZZING] Fix heap-buffer-overflow on REMB dumping

--- src/rtp/RTCPPayloadFeedback.cpp
+++ src/rtp/RTCPPayloadFeedback.cpp
@@ -68,11 +68,17 @@ void RTCPPayloadFeedback::Dump()
 					//Get bitrate
 					DWORD bitrate = mantisa << exp;
 					//Log
-					Debug("\t[REMB bitrate=%d exp=%d mantisa=%d/]\n",bitrate,exp,mantisa);
+					Debug("\t[REMB bitrate=%d exp=%d mantisa=%d num=%d/]\n",bitrate,exp,mantisa,num);
 					//For each
 					for (DWORD i=0;i<num;++i)
+					{
+						//Check length
+						if (len<8+4*i+4)
+							//wrong format
+							break;
 						//Log
 						Debug("\t[ssrc=%u/]\n",get4(payload,8+4*i));
+					}
 					//Log
 					Debug("\t[/REMB]\n");
 					
