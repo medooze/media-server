b4d9f00b3409a8ca19c875b6782856ca09311946
josemrecio <josemrecio@gmail.com>
2019-01-07 16:03:00 +0800

    Check for all possible extensions
    Addresses #49

--- include/rtp/RTPPacket.h
+++ include/rtp/RTPPacket.h
@@ -129,7 +129,12 @@ public:
 	const RTPHeaderExtension&	GetRTPHeaderExtension()	const { return extension;	}
 	
 protected:
-	void  CheckExtensionMark()	{ header.extension =  extension.hasAbsSentTime || extension.hasTimeOffset  || extension.hasTransportWideCC ||  extension.hasFrameMarking; }
+	void  CheckExtensionMark()	{ header.extension =  extension.hasAudioLevel
+						|| extension.hasAbsSentTime 
+						|| extension.hasTimeOffset
+						|| extension.hasTransportWideCC
+						||  extension.hasFrameMarking; }
+					// add checks for hasRId, hasRepairedId and hasMediaStreamId?
 
 private:
 	static const DWORD SIZE = 1700;
