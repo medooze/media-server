f85f7308ab3f58aea35679559471be8275f69621
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2019-01-18 11:46:13 +0100

    Fix Makefile compilation

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 #define VERSION_H
-#define MCUVERSION "5bcd6b2"
-#define MCUDATE "Wed Dec 19 12:17:50 2018 +0100"
+#define MCUVERSION "020b66d"
+#define MCUDATE "Fri Jan 18 10:38:37 2019 +0100"
 #endif
 
