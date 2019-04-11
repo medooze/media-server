72d7509cfe4203b9ab6e4ba680cb06c62af3683f
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2019-04-11 10:40:43 +0200

    Fix comile

--- include/rtp/RTPSource.h
+++ include/rtp/RTPSource.h
@@ -26,8 +26,8 @@ struct LayerInfo
 	BYTE temporalLayerId = MaxLayerId;
 	BYTE spatialLayerId  = MaxLayerId;
 	
-	bool IsValid() { return spatialLayerId!=MaxLayerId || temporalLayerId != MaxLayerId;	}
-	WORD GetId()   { return ((WORD)spatialLayerId)<<8  | temporalLayerId;			}
+	bool IsValid() const { return spatialLayerId!=MaxLayerId || temporalLayerId != MaxLayerId;	}
+	WORD GetId()   const { return ((WORD)spatialLayerId)<<8  | temporalLayerId;			}
 };
 
 struct LayerSource : LayerInfo
