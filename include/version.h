d54241d70bab954f64c32a43aba24f2e7a3dec95
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2019-05-30 12:36:36 +0200

    Fix locking between mixer and pipes

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 #define VERSION_H
-#define MCUVERSION "564ec9e"
-#define MCUDATE "Thu May 9 10:08:50 2019 +0200"
+#define MCUVERSION "bd84dd5"
+#define MCUDATE "Thu May 30 11:05:49 2019 +0200"
 #endif
 
