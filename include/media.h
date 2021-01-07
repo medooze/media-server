5f009c9136c4b504b4fd249206981bbbd1cff1ca
Marcel Hencke <7267954+marcelhencke@users.noreply.github.com>
2021-01-07 14:06:31 +0100

    SWIG go compiler issues with overloaded methods (#106)
    Conditional compiling to fix function signature for SWIG GO compiler

--- include/media.h
+++ include/media.h
@@ -174,7 +174,11 @@ public:
 
 	DWORD GetLength() const			{ return buffer->GetSize();			}
 	DWORD GetMaxMediaLength() const		{ return buffer->GetCapacity();			}
+
+#ifndef SWIGGO
+	// the SWIG compiler can not handle correctly the 2 GetData signatures for the GoLang target
 	const BYTE* GetData() const		{ return buffer->GetData();			}
+#endif
 
 	BYTE* GetData()				{ AdquireBuffer(); return buffer->GetData();	}
 	void SetLength(DWORD length)		{ AdquireBuffer(); buffer->SetSize(length);	}
