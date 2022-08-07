a4ae7870bc207c45dc83052afda5431835c8c8cf
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2022-08-07 20:03:02 +0200

    Fix checksum

--- include/CheckSum.h
+++ include/CheckSum.h
@@ -7,25 +7,27 @@
 class CheckSum
 {
 public:
-	inline void Calculate(const char* data, size_t size)
-	{
-		for (size_t i = 0; i < size; i = i + 2)
-		{
-			uint16_t word;
-			memcpy(&word, ((char*)data) + i, sizeof word);
-			checksum += word;
-		}
-		if (size % 2 != 0)
-			checksum += uint16_t(data[size - 1]);
-	}
-	inline uint32_t Finalize()
-	{
-		checksum = (checksum & 0xFFFF) + (checksum >> 16);
-		checksum = (checksum & 0xFFFF) + (checksum >> 16);
-		checksum = ~checksum;
-		if (!checksum) checksum = 0xFFFF;
-		return checksum;
-	}
+        inline void Calculate(const char* data, size_t size)
+        {
+                for (size_t i = 0; i < (size & ~1); i = i + 2)
+                {
+                        uint16_t word;
+                        memcpy(&word, data + i, sizeof word);
+                        checksum += word;
+                }
+                if (size % 2 != 0)
+                        checksum += ((uint8_t*)data)[size - 1];
+        }
+	
+        inline uint16_t Finalize()
+        {
+                checksum = (checksum & 0xFFFF) + (checksum >> 16);
+                checksum = (checksum & 0xFFFF) + (checksum >> 16);
+                uint32_t res = ~checksum;
+                if (!res) res = 0xFFFF;
+                return res;
+        }
+
 private:
 	uint32_t checksum = 0;
 };
