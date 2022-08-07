fdf4fb8a673bc7844c39d048a436b517e2f56e74
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2022-08-07 20:05:45 +0200

    spaces ->tabs

--- include/CheckSum.h
+++ include/CheckSum.h
@@ -7,26 +7,26 @@
 class CheckSum
 {
 public:
-        inline void Calculate(const char* data, size_t size)
-        {
-                for (size_t i = 0; i < (size & ~1); i = i + 2)
-                {
-                        uint16_t word;
-                        memcpy(&word, data + i, sizeof word);
-                        checksum += word;
-                }
-                if (size % 2 != 0)
-                        checksum += ((uint8_t*)data)[size - 1];
-        }
+	inline void Calculate(const char* data, size_t size)
+	{
+		for (size_t i = 0; i < (size & ~1); i = i + 2)
+		{
+			uint16_t word;
+			memcpy(&word, data + i, sizeof word);
+			checksum += word;
+		}
+		if (size % 2 != 0)
+			checksum += ((uint8_t*)data)[size - 1];
+	}
 	
-        inline uint16_t Finalize()
-        {
-                checksum = (checksum & 0xFFFF) + (checksum >> 16);
-                checksum = (checksum & 0xFFFF) + (checksum >> 16);
-                uint32_t res = ~checksum;
-                if (!res) res = 0xFFFF;
-                return res;
-        }
+	inline uint16_t Finalize()
+	{
+		checksum = (checksum & 0xFFFF) + (checksum >> 16);
+		checksum = (checksum & 0xFFFF) + (checksum >> 16);
+		uint32_t res = ~checksum;
+		if (!res) res = 0xFFFF;
+		return res;
+	}
 
 private:
 	uint32_t checksum = 0;
