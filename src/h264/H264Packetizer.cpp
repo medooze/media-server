e34678b74f323428dc9ebe9850ba08d554e41c9c
Alba Mendez <alba.mendez@dolby.com>
2023-06-28 00:29:00 +0200

    extract common packetizer code into H26xPacketizer

--- src/h264/H264Packetizer.cpp
+++ src/h264/H264Packetizer.cpp
@@ -3,22 +3,14 @@
 #include "video.h"
 #include "h264/h264.h"
 
-void OnH264Nal(VideoFrame& frame, AVCDescriptor &config, bool& noPPSInFrame, bool& noSPSInFrame, BufferReader& reader)
+void H264Packetizer::OnNal(VideoFrame& frame, BufferReader& reader)
 {
 	//Return if current NAL is empty
 	if (!reader.GetLeft())
 		return;
 
-	//Get nal size
-	uint32_t nalSize = reader.GetLeft();
-	//Get nal unit
-	const uint8_t* nalUnit = reader.PeekData();
-	//Nal data
-	const uint8_t* nalData = nalUnit + 1;
-
-	//Empty prefix
-	uint8_t preffix[4] = {};
-
+	auto nalSize = reader.GetLeft();
+	auto nalUnit = reader.PeekData();
 
 	/* +---------------+
 	 * |0|1|2|3|4|5|6|7|
@@ -26,8 +18,9 @@ void OnH264Nal(VideoFrame& frame, AVCDescriptor &config, bool& noPPSInFrame, boo
 	 * |F|NRI|  Type   |
 	 * +---------------+
 	 */
-	uint8_t nri		= nalUnit[0] & 0b0'11'00000;
-	uint8_t nalUnitType	= nalUnit[0] & 0b0'00'11111;
+	uint8_t naluHeader 	= reader.Get1();
+	uint8_t nri		= naluHeader & 0b0'11'00000;
+	uint8_t nalUnitType	= naluHeader & 0b0'00'11111;
 
 	//UltraDebug("-SRTConnection::OnVideoData() | Got nal [type:%d,size:%d]\n", nalUnitType, nalSize);
 
@@ -64,37 +57,15 @@ void OnH264Nal(VideoFrame& frame, AVCDescriptor &config, bool& noPPSInFrame, boo
 				//UltraDebug("-SRTConnection::OnVideoData() | SPS [num: %d, size: %d], PPS [num: %d, size: %d]\n", spsNum, spsTotalSize, ppsNum, ppsTotalSize);
 				//Allocate enought space to prevent further reallocations
 				// preffix num: spsNum + ps_num + 1
-				frame.Alloc(frame.GetLength() + nalSize + spsTotalSize + ppsTotalSize + sizeof(preffix) * (spsNum + ppsNum + 1));
+				frame.Alloc(frame.GetLength() + nalSize + spsTotalSize + ppsTotalSize + OUT_NALU_LENGTH_SIZE * (spsNum + ppsNum + 1));
 
 				// Add SPS
 				for (uint8_t i = 0; i < spsNum; i++)
-				{
-					const uint8_t* spsData = config.GetSequenceParameterSet(i);
-					uint32_t spsSize = config.GetSequenceParameterSetSize(i);
-					//Set size in preffix
-					set4(preffix, 0, spsSize);
-					//Add nal preffix
-					frame.AppendMedia(preffix, sizeof(preffix));
-					//Add SPS
-					auto pos = frame.AppendMedia(spsData, spsSize);
-					//Single packetization
-					frame.AddRtpPacket(pos, spsSize);
-				}
+					EmitNal(frame, BufferReader(config.GetSequenceParameterSet(i), config.GetSequenceParameterSetSize(i)));
 
 				// Add PPS
 				for (uint8_t i = 0; i < ppsNum; i++)
-				{
-					const uint8_t* ppsData = config.GetPictureParameterSet(i);
-					uint32_t ppsSize = config.GetPictureParameterSetSize(i);
-					//Set size in preffix
-					set4(preffix, 0, ppsSize);
-					//Add nal preffix
-					frame.AppendMedia(preffix, sizeof(preffix));
-					//Add PPS
-					auto pos = frame.AppendMedia(ppsData, ppsSize);
-					//Single packetization
-					frame.AddRtpPacket(pos, ppsSize);
-				}
+					EmitNal(frame, BufferReader(config.GetPictureParameterSet(i), config.GetPictureParameterSetSize(i)));
 
 				//Serialize config in to frame
 				frame.AllocateCodecConfig(config.GetSize());
@@ -107,12 +78,14 @@ void OnH264Nal(VideoFrame& frame, AVCDescriptor &config, bool& noPPSInFrame, boo
 			if (nalSize < 4)
 				break;
 
+			const uint8_t* nalData = reader.PeekData();
+
 			//Set config
 			config.SetConfigurationVersion(1);
 			config.SetAVCProfileIndication(nalData[0]);
 			config.SetProfileCompatibility(nalData[1]);
 			config.SetAVCLevelIndication(nalData[2]);
-			config.SetNALUnitLength(sizeof(preffix) - 1);
+			config.SetNALUnitLength(OUT_NALU_LENGTH_SIZE - 1);
 
 			//Reset previous PPS only on the 1st PPS in current frame
 			if (noSPSInFrame)
@@ -159,94 +132,13 @@ void OnH264Nal(VideoFrame& frame, AVCDescriptor &config, bool& noPPSInFrame, boo
 			return;
 	}
 
-	
-	//Set NAL size on preffix
-	set4(preffix, 0, nalSize);
-
-	//Allocate enought space to prevent further reallocations
-	frame.Alloc(frame.GetLength() + nalSize + sizeof(preffix));
-
-	//Add nal start
-	frame.AppendMedia(preffix, sizeof(preffix));
-
-	//Check nal start in frame
-	auto pos = frame.AppendMedia(reader);
-
-	//If it is small
-	if (nalSize < RTPPAYLOADSIZE)
-	{
-		//Single packetization
-		frame.AddRtpPacket(pos, nalSize);
-		return;
-	}
-
-	//Otherwise use FU-A
-	/*
-	* The FU indicator octet has the following format:
-	*
-	*       +---------------+
-	*       |0|1|2|3|4|5|6|7|
-	*       +-+-+-+-+-+-+-+-+
-	*       |F|NRI|  Type   |
-	*       +---------------+
-	*
-	* The FU header has the following format:
-	*
-	*      +---------------+
-	*      |0|1|2|3|4|5|6|7|
-	*      +-+-+-+-+-+-+-+-+
-	*      |S|E|R|  Type   |
-	*      +---------------+
-	*/
-	uint8_t nalHeader[2] = { nri | 28u, 0b100'00000 | nalUnitType };
-	//Pending uint8_ts
-	uint32_t pending = nalSize;
-	//Skip payload nal header
-	pending--;
-	pos++;
-	//Split it
-	while (pending)
-	{
-		int len = std::min<uint32_t>(RTPPAYLOADSIZE - 2, pending);
-		//Read it
-		pending -= len;
-		//If all added
-		if (!pending)
-			//Set end mark
-			nalHeader[1] |= 0b010'00000;
-		//Add packetization
-		frame.AddRtpPacket(pos, len, nalHeader, sizeof(nalHeader));
-		//Not first
-		nalHeader[1] &= 0b011'11111;
-		//Move start
-		pos += len;
-	}
+	EmitNal(frame, BufferReader(nalUnit, nalSize));
 }
 
 std::unique_ptr<VideoFrame> H264Packetizer::ProcessAU(BufferReader reader)
 {
-	//UltraDebug("-H264Packetizer::ProcessAU() | H264 AU [len:%d]\n", reader.GetLeft());
-	
-	//Alocate enought data
-	auto frame = std::make_unique<VideoFrame>(VideoCodec::H264, reader.GetSize());
-	bool noPPSInFrame = true;
-	bool noSPSInFrame = true;
+	noPPSInFrame = true;
+	noSPSInFrame = true;
 
-	NalSliceAnnexB(std::move(reader), [&](auto reader) {
-		OnH264Nal(*frame, config, noPPSInFrame, noSPSInFrame, reader);
-	});
-
-	//Check if we have new width and heigth
-	if (frame->GetWidth() && frame->GetHeight())
-	{
-		//Update cache
-		width = frame->GetWidth();
-		height = frame->GetHeight();
-	} else {
-		//Update from cache
-		frame->SetWidth(width);
-		frame->SetHeight(height);
-	}
-
-	return frame;
+	return H26xPacketizer::ProcessAU(reader);
 }
--- src/h264/H264Packetizer.h
+++ src/h264/H264Packetizer.h
@@ -1,27 +1,39 @@
 #ifndef H264PACKETIZER_H
 #define H264PACKETIZER_H
 
-#include "video.h"
+#include "H26xPacketizer.h"
 #include "avcdescriptor.h"
 
 /**
  * RTP packetizer for H.264 streams.
  * 
- * packetization involves undoing Annex B byte stream (slicing NALUs, ignoring AUDs),
+ * packetization involves undoing incoming NAL stream (slicing NALUs, ignoring AUDs),
  * and retaining SPS/PPS to then inject on each keyframe as required by WebRTC.
- * it also drops frames until the first SPS/PPS are received.
  */
-class H264Packetizer
+class H264Packetizer : public H26xPacketizer
 {
 public:
-	std::unique_ptr<VideoFrame> ProcessAU(BufferReader payload);
 
-	// this is not private because MPEGTSAVCStream needs to access it
-	// to know when to start allowing frames through
+	/** this is not private because MPEGTSAVCStream needs to access it
+	    to know when to start allowing frames through */
 	AVCDescriptor config;
-private:
-	uint32_t width = 0;
-	uint32_t height = 0;
+
+	std::unique_ptr<VideoFrame> ProcessAU(BufferReader reader) override;
+
+protected:
+	void OnNal(VideoFrame& frame, BufferReader& nal) override;
+
+	void EmitNal(VideoFrame& frame, BufferReader nal)
+	{
+		uint8_t naluHeader 	= nal.Peek1();
+		uint8_t nri		= naluHeader & 0b0'11'00000;
+		uint8_t nalUnitType	= naluHeader & 0b0'00'11111;
+
+		std::string fuPrefix = { (char)(nri | 28u), (char)nalUnitType };
+		H26xPacketizer::EmitNal(frame, nal, fuPrefix, 1);
+	}
+
+	bool noPPSInFrame, noSPSInFrame;
 };
 
 #endif // H264PACKETIZER_H
