08e9103cc58de509557369bcb89ebffd6d9259bc
Alba Mendez <alba.mendez@dolby.com>
2023-06-27 17:19:31 +0200

    remove duplicated h.265 code

--- src/h265/h265.h
+++ src/h265/h265.h
@@ -414,24 +414,5 @@ private:
 
 #undef CHECK
 
-inline void	H265ToAnnexB(BYTE* data, DWORD size)
-{
-	DWORD pos =	0;
-
-	while (pos < size)
-	{
-		//Get nal size
-		DWORD nalSize =	get4(data, pos);
-		//If it	was	already	in annex B
-		if (nalSize==1 && !pos)
-			//Done
-			return;
-		//Set annexB start code
-		set4(data, pos,	1);
-		//Next
-		pos	+= 4 + nalSize;
-	}
-}
-
 #endif	/* H265_H */
 
