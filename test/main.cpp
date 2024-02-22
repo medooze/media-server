0900911646264b2bad05329e65e7c39818fe78ea
Alba Mendez <alba.mendez@dolby.com>
2024-02-22 14:24:14 +0100

    allow running a subset of the tests

--- test/main.cpp
+++ test/main.cpp
@@ -6,12 +6,23 @@ extern "C" {
 #include "libavcodec/avcodec.h"
 #include "libavformat/avformat.h"
 }
+#include <algorithm>
+#include <vector>
+#include <string>
 
 
 int main(int argc, char** argv)
 {
 	Logger::EnableDebug(true);
 
-	return TestPlan::ExecuteAll();
+	std::vector<std::string> args;
+	for (int i = 1; i < argc; i++)
+		args.push_back(argv[i]);
+
+	return TestPlan::ExecuteAll([&](const TestPlan& test) {
+		if (args.empty())
+			return true;
+		return std::find(args.begin(), args.end(), test.GetName()) != args.end();
+	});
 }
 
