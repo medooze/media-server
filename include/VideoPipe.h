6757ba5a0b35b4652b81e1ba710ef780f5ec8d9c
Brendon Costa <bcost@dolby.com>
2024-02-23 08:45:06 +1100

    Cleanup the VideoPipe stats

--- include/VideoPipe.h
+++ include/VideoPipe.h
@@ -54,9 +54,9 @@ private:
 	VideoBufferScaler scaler;
 	AllowedDownScaling allowedDownScaling = AllowedDownScaling::Any;
 
-	size_t totalDroppedFrames = 0;
 	uint64_t lastDroppedReport = 0;
-	Acumulator<uint32_t, uint64_t> droppedFramesAcu;
+	unsigned int droppedFramesSinceReport = 0;
+	unsigned int totalFramesSinceReport = 0;
 };
 
 #endif	/* VIDEOPIPE_H */
