231be4dac57d7ae8d4562b0c3fc12fc870cb735e
Harry Zhang <harry.zhang@dolby.com>
2024-07-03 11:34:16 +1000

    Remove itself when detaching from producer

--- src/SimulcastMediaFrameListener.cpp
+++ src/SimulcastMediaFrameListener.cpp
@@ -77,6 +77,7 @@ void SimulcastMediaFrameListener::Detach(const MediaFrame::Producer::shared& pro
 {
 	Debug("-SimulcastMediaFrameListener::Detach() [this:%p,producer:%p]\n", this, producer.get());
 	timeService.Sync([=](std::chrono::milliseconds) {
+		producer->RemoveMediaListener(shared_from_this());
 		producers.erase(producer);
 	});
 }
