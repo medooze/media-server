3f191b5831ac46652a39220eb8a6c108381beda7
Brendon Costa <bcost@dolby.com>
2024-07-31 10:09:42 +1000

    Fixed typo

--- include/rtmp/amf.h
+++ include/rtmp/amf.h
@@ -309,7 +309,7 @@ private:
 	// Prevent future problems with shallow copies for now by preventing them
 	// Maybe later we will support deep copying of "elements"
 	AMFStrictArray(const AMFStrictArray& rhs) = delete;
-	AMFStrictArray* operator=(const AMFStrictArray& rhs) = delete;
+	AMFStrictArray& operator=(const AMFStrictArray& rhs) = delete;
 
 	AMFData** elements;
 	AMFParser parser;
