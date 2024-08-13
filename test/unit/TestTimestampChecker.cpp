0c596be5a640c0ef8bd31eb723a1a68bcdcee580
Harry Zhang <harry.zhang@dolby.com>
2024-08-13 14:37:25 +1000

    Add test case for real timing info

--- test/unit/TestTimestampChecker.cpp
+++ test/unit/TestTimestampChecker.cpp
@@ -2,7 +2,18 @@
 #include "TimestampChecker.h"
 #include "data/FramesArrivalInfo.h"
 
-TEST(TestTimestampChecker, NormalStream)
+class TestTimestampChecker : public ::testing::Test
+{
+public:
+	
+	void CheckExpectedTimestamp(uint64_t ts, uint64_t time, uint64_t lastTs, uint64_t lastTime, uint32_t clk, uint64_t expectedMaxDiff)
+	{
+		auto expectedTs = lastTs + (time - lastTime) * clk / 1000;
+		ASSERT_LE(fabs(int64_t(ts) - int64_t(expectedTs)), expectedMaxDiff);
+	}
+};
+
+TEST_F(TestTimestampChecker, NormalStream)
 {
 	std::unordered_map<MediaFrame::Type, TimestampChecker> checkers;
 	
@@ -21,7 +32,7 @@ TEST(TestTimestampChecker, NormalStream)
 	}
 }
 
-TEST(TestTimestampChecker, TimestampSpike)
+TEST_F(TestTimestampChecker, TimestampSpike)
 {
 	std::unordered_map<MediaFrame::Type, TimestampChecker> checkers;
 	
@@ -53,7 +64,7 @@ TEST(TestTimestampChecker, TimestampSpike)
 	}
 }
 
-TEST(TestTimestampChecker, FirstTimeStampWrong)
+TEST_F(TestTimestampChecker, FirstTimeStampWrong)
 {
 	std::unordered_map<MediaFrame::Type, TimestampChecker> checkers;
 	std::unordered_map<MediaFrame::Type, uint32_t> counters;
@@ -73,7 +84,7 @@ TEST(TestTimestampChecker, FirstTimeStampWrong)
 			auto [result, rts] = checker.Check(time, ts, clk);
 			ASSERT_EQ(TimestampChecker::CheckResult::Valid, result);
 			
-			EXPECT_EQ(0, checker.GetTimestampOffset());
+			ASSERT_EQ(0, checker.GetTimestampOffset());
 		}
 		else if (counter > 0 && counter < (1 + TimestampChecker::DefaultMaxContinousInvalidFrames))
 		{
@@ -103,12 +114,14 @@ TEST(TestTimestampChecker, FirstTimeStampWrong)
 	}
 }
 
-TEST(TestTimestampChecker, TimeStampWrong)
+TEST_F(TestTimestampChecker, TimeStampWrong)
 {
 	std::unordered_map<MediaFrame::Type, TimestampChecker> checkers;
 	std::unordered_map<MediaFrame::Type, uint32_t> counters;
 	
+	std::unordered_map<MediaFrame::Type, uint64_t> lastTime;
 	std::unordered_map<MediaFrame::Type, uint64_t> lastRts;
+	
 	for (auto [type, time, ts, clk] : TestData::FramesArrivalInfo)
 	{
 		std::stringstream ss;
@@ -171,10 +184,74 @@ TEST(TestTimestampChecker, TimeStampWrong)
 		auto maxDiff = clk * 0.3;
 		if (lastRts.find(type) != lastRts.end())
 		{
-			ASSERT_LE(fabs(rts - lastRts[type]), maxDiff);
+			ASSERT_NO_FATAL_FAILURE(CheckExpectedTimestamp(rts, time, lastRts[type], lastTime[type], clk, maxDiff));
 		}
 		
 		lastRts[type] = rts;
+		lastTime[type] = time;
+		
+		counters[type]++;
+	}
+}
+
+TEST_F(TestTimestampChecker, TimestampBaseChange)
+{
+	std::unordered_map<MediaFrame::Type, TimestampChecker> checkers;
+	
+	std::unordered_map<MediaFrame::Type, size_t> counters;
+	
+	std::unordered_map<MediaFrame::Type, size_t> timestampChangeIndices;
+	timestampChangeIndices[MediaFrame::Type::Audio] = 37;
+	timestampChangeIndices[MediaFrame::Type::Video] = 24;
+	
+	std::unordered_map<MediaFrame::Type, uint64_t> lastTime;
+	std::unordered_map<MediaFrame::Type, uint64_t> lastRts;
+	
+	for (auto [type, time, ts, clk] : TestData::FramesArrivalInfoTimestampChange)
+	{	
+		std::stringstream ss;
+		ss << counters[type] << ": type: " << type << " time: " << time << " ts: " << ts;
+		SCOPED_TRACE(ss.str());
+		
+		auto& checker = checkers[type];
+		
+		auto counter = counters[type];
+		
+		auto [result, rts] = checker.Check(time, ts, clk);
+		
+		if (counter < timestampChangeIndices[type])
+		{
+			ASSERT_EQ(TimestampChecker::CheckResult::Valid, result);
+			ASSERT_EQ(ts, rts);
+			ASSERT_EQ(0, checker.GetTimestampOffset());
+		}
+		else if (counter < timestampChangeIndices[type] + TimestampChecker::DefaultMaxContinousInvalidFrames)
+		{
+			ASSERT_EQ(TimestampChecker::CheckResult::Invalid, result);
+			ASSERT_EQ(0, checker.GetTimestampOffset());
+		}
+		else if (counter == timestampChangeIndices[type] + TimestampChecker::DefaultMaxContinousInvalidFrames)
+		{
+			ASSERT_EQ(TimestampChecker::CheckResult::Reset, result);
+			ASSERT_EQ(type == MediaFrame::Type::Audio ? 202526992 : 379729170, checker.GetTimestampOffset());
+		}
+		else
+		{
+			ASSERT_EQ(TimestampChecker::CheckResult::Valid, result);
+			ASSERT_EQ(type == MediaFrame::Type::Audio ? 202526992 : 379729170, checker.GetTimestampOffset());
+		}
+		
+		// The timestamp should be mostly incremental with a resonable value.
+		// We don't check incremental here as the calculated one might be not extactly same as the
+		// one recieved from ingest due to recieving time delay
+		auto maxDiff = clk * 0.3;
+		if (lastRts.find(type) != lastRts.end())
+		{
+			ASSERT_NO_FATAL_FAILURE(CheckExpectedTimestamp(rts, time, lastRts[type], lastTime[type], clk, maxDiff));
+		}
+		
+		lastRts[type] = rts;
+		lastTime[type] = time;
 		
 		counters[type]++;
 	}
