530b1a6e5fa28e0c57ff8d6d067245df2d086374
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2024-08-29 18:05:52 +0200

    feat: Add enhanced RTMP multitrack support (#329)

--- include/rtmp/rtmppacketizer.h
+++ include/rtmp/rtmppacketizer.h
@@ -12,13 +12,43 @@
 #include <memory>
 
 VideoCodec::Type GetRtmpFrameVideoCodec(const RTMPVideoFrame& videoFrame);
+AudioCodec::Type GetRtmpFrameAudioCodec(const RTMPAudioFrame& audioFrame);
 
-template<typename DescClass, VideoCodec::Type codec>
-class RTMPPacketizer
+class RTMPAudioPacketizer
 {
 public:
-	virtual std::unique_ptr<VideoFrame> AddFrame(RTMPVideoFrame* videoFrame) = 0;
+	RTMPAudioPacketizer(AudioCodec::Type type) :
+		type(type)
+	{}
+	AudioCodec::Type GetCodec() const { return type; }
+	virtual std::unique_ptr<AudioFrame> AddFrame(RTMPAudioFrame* videoFrame) = 0;
+private:
+	AudioCodec::Type type;
+};
 
+class RTMPVideoPacketizer
+{
+public:
+	RTMPVideoPacketizer(VideoCodec::Type type) :
+		type(type)
+	{}
+	VideoCodec::Type GetCodec() const { return type; }
+	virtual std::unique_ptr<VideoFrame> AddFrame(RTMPVideoFrame* videoFrame) = 0;
+private:
+	VideoCodec::Type type;
+};
+
+std::unique_ptr<RTMPVideoPacketizer> CreateRTMPVideoPacketizer(VideoCodec::Type type);
+std::unique_ptr<RTMPAudioPacketizer> CreateRTMPAudioPacketizer(AudioCodec::Type type);
+
+
+template<typename DescClass, VideoCodec::Type codec>
+class RTMPVideoPacketizerImpl : public RTMPVideoPacketizer
+{
+public:
+	RTMPVideoPacketizerImpl() :
+		RTMPVideoPacketizer(codec)
+	{}
 protected:
 	
 	std::unique_ptr<VideoFrame> PrepareFrame(RTMPVideoFrame* videoFrame);
@@ -29,7 +59,7 @@ protected:
 
 
 template<typename DescClass, typename SPSClass, typename PPSClass, VideoCodec::Type codec>
-class RTMPH26xPacketizer : public RTMPPacketizer<DescClass, codec>
+class RTMPH26xPacketizer : public RTMPVideoPacketizerImpl<DescClass, codec>
 {
 public:
 	virtual std::unique_ptr<VideoFrame> AddFrame(RTMPVideoFrame* videoFrame) override;
@@ -41,33 +71,36 @@ public:
 using RTMPAVCPacketizer = RTMPH26xPacketizer<AVCDescriptor, H264SeqParameterSet, H264PictureParameterSet, VideoCodec::H264>;
 using RTMPHEVCPacketizer = RTMPH26xPacketizer<HEVCDescriptor, H265SeqParameterSet, H265PictureParameterSet, VideoCodec::H265>;
 
-class RTMPAv1Packetizer : public RTMPPacketizer<AV1CodecConfigurationRecord, VideoCodec::AV1>
+class RTMPAv1Packetizer : public RTMPVideoPacketizerImpl<AV1CodecConfigurationRecord, VideoCodec::AV1>
 {
 public:
 	virtual std::unique_ptr<VideoFrame> AddFrame(RTMPVideoFrame* videoFrame) override;
 };
 
 
-class RTMPAACPacketizer
+class RTMPAACPacketizer : public RTMPAudioPacketizer
 {
-public:
-	std::unique_ptr<AudioFrame> AddFrame(RTMPAudioFrame* videoFrame);
+public: 
+	RTMPAACPacketizer() : RTMPAudioPacketizer(AudioCodec::AAC) {}
+	virtual std::unique_ptr<AudioFrame> AddFrame(RTMPAudioFrame* videoFrame) override;
 private:
 	AACSpecificConfig aacSpecificConfig;
         bool gotConfig = false;
 };
 
 
-class RTMPG711APacketizer
+class RTMPG711APacketizer : public RTMPAudioPacketizer
 {
 public:
-	std::unique_ptr<AudioFrame> AddFrame(RTMPAudioFrame* videoFrame);
+	RTMPG711APacketizer() : RTMPAudioPacketizer(AudioCodec::PCMA) {}
+	virtual std::unique_ptr<AudioFrame> AddFrame(RTMPAudioFrame* videoFrame) override;
 };
 
-class RTMPG711UPacketizer
+class RTMPG711UPacketizer : public RTMPAudioPacketizer
 {
 public:
-	std::unique_ptr<AudioFrame> AddFrame(RTMPAudioFrame* videoFrame);
+	RTMPG711UPacketizer() : RTMPAudioPacketizer(AudioCodec::PCMU) { }
+	virtual std::unique_ptr<AudioFrame> AddFrame(RTMPAudioFrame* videoFrame) override;
 };
 
 #endif /* RTMPPACKETIZER_H */
