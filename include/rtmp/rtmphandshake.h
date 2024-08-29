3b60386aa0c8cc2f09731bc3c2a08ef9221a6772
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2024-08-29 17:47:23 +0200

    Update HMAC for openssl 3 api

--- include/rtmp/rtmphandshake.h
+++ include/rtmp/rtmphandshake.h
@@ -55,12 +55,15 @@ static void HMACsha256(const uint8_t *message, size_t messageLen, const uint8_t 
 {
 	unsigned int digestLen;
 	
+#if OPENSSL_VERSION_NUMBER > 0x30000000L
+	HMAC(EVP_sha256(), key, keylen, message, messageLen, digest, &digestLen);
+#else
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
-  HMAC_CTX ctx;
-  HMAC_CTX_init(&ctx); 
+	HMAC_CTX ctx;
+	HMAC_CTX_init(&ctx); 
 	HMAC_Init_ex(&ctx, key, keylen, EVP_sha256(), 0);
 	HMAC_Update(&ctx, message, messageLen);
-	HMAC_Final(&ctx, digest, &digestLen);
+	HMAC_Final(&ctx, );
 	HMAC_CTX_cleanup(&ctx);
 #else
 	HMAC_CTX *ctx = HMAC_CTX_new();
@@ -68,6 +71,7 @@ static void HMACsha256(const uint8_t *message, size_t messageLen, const uint8_t 
 	HMAC_Update(ctx, message, messageLen);
 	HMAC_Final(ctx, digest, &digestLen);
 	HMAC_CTX_free(ctx);
+#endif
 #endif
 
 }
