88184bc7521d71c5fa5a5ca1891cabcc1b42e70d
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2024-09-27 11:56:09 +0200

    lint: spaces to tabs

--- test/unit/TestH26xPPS.cpp
+++ test/unit/TestH26xPPS.cpp
@@ -4,8 +4,7 @@
 
 TEST(TestH26xPPS, TestParsePPS1)
 {
-	
-	const std::vector<BYTE> ppsData = {0xc8, 0x42, 0x02, 0x32, 0xc8};
+	const std::vector<BYTE> ppsData = { 0xc8, 0x42, 0x02, 0x32, 0xc8 };
 
 
 	H264PictureParameterSet ppsParser;
@@ -13,29 +12,29 @@ TEST(TestH26xPPS, TestParsePPS1)
 	ppsParser.Decode(ppsData.data(), ppsData.size());
 	// ppsParser.Dump();
 
-    EXPECT_EQ(0, ppsParser.pic_parameter_set_id);
-    EXPECT_EQ(0, ppsParser.seq_parameter_set_id);
-    EXPECT_EQ(0, ppsParser.entropy_coding_mode_flag);
-    EXPECT_EQ(0, ppsParser.pic_order_present_flag);
-    EXPECT_EQ(0, ppsParser.num_slice_groups_minus1);
-    EXPECT_EQ(15, ppsParser.num_ref_idx_l0_active_minus1);
-    EXPECT_EQ(0, ppsParser.num_ref_idx_l1_active_minus1);
-    EXPECT_EQ(0, ppsParser.weighted_pred_flag);
-    EXPECT_EQ(0, ppsParser.weighted_bipred_idc);
-    EXPECT_EQ(-8, ppsParser.pic_init_qp_minus26);
-    EXPECT_EQ(0, ppsParser.pic_init_qs_minus26);
-    EXPECT_EQ(-2, ppsParser.chroma_qp_index_offset);
-    EXPECT_EQ(1, ppsParser.deblocking_filter_control_present_flag);
-    EXPECT_EQ(0, ppsParser.constrained_intra_pred_flag);
-    EXPECT_EQ(0, ppsParser.redundant_pic_cnt_present_flag);
+	EXPECT_EQ(0, ppsParser.pic_parameter_set_id);
+	EXPECT_EQ(0, ppsParser.seq_parameter_set_id);
+	EXPECT_EQ(0, ppsParser.entropy_coding_mode_flag);
+	EXPECT_EQ(0, ppsParser.pic_order_present_flag);
+	EXPECT_EQ(0, ppsParser.num_slice_groups_minus1);
+	EXPECT_EQ(15, ppsParser.num_ref_idx_l0_active_minus1);
+	EXPECT_EQ(0, ppsParser.num_ref_idx_l1_active_minus1);
+	EXPECT_EQ(0, ppsParser.weighted_pred_flag);
+	EXPECT_EQ(0, ppsParser.weighted_bipred_idc);
+	EXPECT_EQ(-8, ppsParser.pic_init_qp_minus26);
+	EXPECT_EQ(0, ppsParser.pic_init_qs_minus26);
+	EXPECT_EQ(-2, ppsParser.chroma_qp_index_offset);
+	EXPECT_EQ(1, ppsParser.deblocking_filter_control_present_flag);
+	EXPECT_EQ(0, ppsParser.constrained_intra_pred_flag);
+	EXPECT_EQ(0, ppsParser.redundant_pic_cnt_present_flag);
 
 
 }
 
 TEST(TestH26xPPS, TestParsePPS2)
 {
-	
-	const std::vector<BYTE> ppsData = {0xe8, 0x43, 0x82, 0x92, 0xc8, 0xb0};
+
+	const std::vector<BYTE> ppsData = { 0xe8, 0x43, 0x82, 0x92, 0xc8, 0xb0 };
 
 
 	H264PictureParameterSet ppsParser;
@@ -43,26 +42,26 @@ TEST(TestH26xPPS, TestParsePPS2)
 	ppsParser.Decode(ppsData.data(), ppsData.size());
 	// ppsParser.Dump();
 
-    EXPECT_EQ(0, ppsParser.pic_parameter_set_id);
-    EXPECT_EQ(0, ppsParser.seq_parameter_set_id);
-    EXPECT_EQ(1, ppsParser.entropy_coding_mode_flag);
-    EXPECT_EQ(0, ppsParser.pic_order_present_flag);
-    EXPECT_EQ(0, ppsParser.num_slice_groups_minus1);
-    EXPECT_EQ(15, ppsParser.num_ref_idx_l0_active_minus1);
-    EXPECT_EQ(0, ppsParser.num_ref_idx_l1_active_minus1);
-    EXPECT_EQ(1, ppsParser.weighted_pred_flag);
-    EXPECT_EQ(2, ppsParser.weighted_bipred_idc);
-    EXPECT_EQ(10, ppsParser.pic_init_qp_minus26);
-    EXPECT_EQ(0, ppsParser.pic_init_qs_minus26);
-    EXPECT_EQ(-2, ppsParser.chroma_qp_index_offset);
-    EXPECT_EQ(1, ppsParser.deblocking_filter_control_present_flag);
-    EXPECT_EQ(0, ppsParser.constrained_intra_pred_flag);
-    EXPECT_EQ(0, ppsParser.redundant_pic_cnt_present_flag);
-    // We are not yet parsing these params? leaving it here as a comment for future reference
-    // EXPECT_EQ(1, ppsParser.transform_8x8_mode_flag);
-    // EXPECT_EQ(0, ppsParser.pic_scaling_matrix_present_flag);
-    // EXPECT_EQ(-2, ppsParser.second_chroma_qp_index_offset);
+	EXPECT_EQ(0, ppsParser.pic_parameter_set_id);
+	EXPECT_EQ(0, ppsParser.seq_parameter_set_id);
+	EXPECT_EQ(1, ppsParser.entropy_coding_mode_flag);
+	EXPECT_EQ(0, ppsParser.pic_order_present_flag);
+	EXPECT_EQ(0, ppsParser.num_slice_groups_minus1);
+	EXPECT_EQ(15, ppsParser.num_ref_idx_l0_active_minus1);
+	EXPECT_EQ(0, ppsParser.num_ref_idx_l1_active_minus1);
+	EXPECT_EQ(1, ppsParser.weighted_pred_flag);
+	EXPECT_EQ(2, ppsParser.weighted_bipred_idc);
+	EXPECT_EQ(10, ppsParser.pic_init_qp_minus26);
+	EXPECT_EQ(0, ppsParser.pic_init_qs_minus26);
+	EXPECT_EQ(-2, ppsParser.chroma_qp_index_offset);
+	EXPECT_EQ(1, ppsParser.deblocking_filter_control_present_flag);
+	EXPECT_EQ(0, ppsParser.constrained_intra_pred_flag);
+	EXPECT_EQ(0, ppsParser.redundant_pic_cnt_present_flag);
+	// We are not yet parsing these params? leaving it here as a comment for future reference
+	// EXPECT_EQ(1, ppsParser.transform_8x8_mode_flag);
+	// EXPECT_EQ(0, ppsParser.pic_scaling_matrix_present_flag);
+	// EXPECT_EQ(-2, ppsParser.second_chroma_qp_index_offset);
 
-	
+
 
 }
--- test/unit/TestH26xSPS.cpp
+++ test/unit/TestH26xSPS.cpp
@@ -4,9 +4,11 @@
 
 TEST(TestH26xSPS, TestParseSPS1)
 {
-	const std::vector<BYTE> spsData = {0x42, 0xc0, 0x16, 0xa6, 0x11, 0x05, 0x07, 0xe9,
-      0xb2, 0x00, 0x00, 0x03, 0x00, 0x02, 0x00, 0x00,
-      0x03, 0x00, 0x64, 0x1e, 0x2c, 0x5c, 0x23, 0x00};
+	const std::vector<BYTE> spsData = {
+		0x42, 0xc0, 0x16, 0xa6, 0x11, 0x05, 0x07, 0xe9,
+		0xb2, 0x00, 0x00, 0x03, 0x00, 0x02, 0x00, 0x00,
+		0x03, 0x00, 0x64, 0x1e, 0x2c, 0x5c, 0x23, 0x00
+	};
 
 
 	H264SeqParameterSet spsParser;
@@ -14,31 +16,33 @@ TEST(TestH26xSPS, TestParseSPS1)
 	spsParser.Decode(spsData.data(), spsData.size());
 	// spsParser.Dump();
 
-    EXPECT_EQ(66, spsParser.profile_idc);
-    EXPECT_EQ(1, spsParser.constraint_set0_flag);
-    EXPECT_EQ(1, spsParser.constraint_set1_flag);
-    EXPECT_EQ(0, spsParser.constraint_set2_flag);
-    EXPECT_EQ(0, spsParser.reserved_zero_5bits);
-    EXPECT_EQ(22, spsParser.level_idc);
-    EXPECT_EQ(0, spsParser.seq_parameter_set_id);
-    EXPECT_EQ(1, spsParser.log2_max_frame_num_minus4);
-    EXPECT_EQ(2, spsParser.pic_order_cnt_type);
-    EXPECT_EQ(16, spsParser.num_ref_frames);
-    EXPECT_EQ(0, spsParser.gaps_in_frame_num_value_allowed_flag);
-    EXPECT_EQ(19, spsParser.pic_width_in_mbs_minus1);
-    EXPECT_EQ(14, spsParser.pic_height_in_map_units_minus1);
-    EXPECT_EQ(1, spsParser.frame_mbs_only_flag);
-    EXPECT_EQ(1, spsParser.direct_8x8_inference_flag);
-    EXPECT_EQ(0, spsParser.frame_cropping_flag);
+	EXPECT_EQ(66, spsParser.profile_idc);
+	EXPECT_EQ(1, spsParser.constraint_set0_flag);
+	EXPECT_EQ(1, spsParser.constraint_set1_flag);
+	EXPECT_EQ(0, spsParser.constraint_set2_flag);
+	EXPECT_EQ(0, spsParser.reserved_zero_5bits);
+	EXPECT_EQ(22, spsParser.level_idc);
+	EXPECT_EQ(0, spsParser.seq_parameter_set_id);
+	EXPECT_EQ(1, spsParser.log2_max_frame_num_minus4);
+	EXPECT_EQ(2, spsParser.pic_order_cnt_type);
+	EXPECT_EQ(16, spsParser.num_ref_frames);
+	EXPECT_EQ(0, spsParser.gaps_in_frame_num_value_allowed_flag);
+	EXPECT_EQ(19, spsParser.pic_width_in_mbs_minus1);
+	EXPECT_EQ(14, spsParser.pic_height_in_map_units_minus1);
+	EXPECT_EQ(1, spsParser.frame_mbs_only_flag);
+	EXPECT_EQ(1, spsParser.direct_8x8_inference_flag);
+	EXPECT_EQ(0, spsParser.frame_cropping_flag);
 
 }
 
 TEST(TestH26xSPS, TestParseSPS2)
 {
-	const std::vector<BYTE> spsData = {0x64, 0x00, 0x33, 0xac, 0x72, 0x84, 0x40, 0x78,
-      0x02, 0x27, 0xe5, 0xc0, 0x44, 0x00, 0x00, 0x03,
-      0x00, 0x04, 0x00, 0x00, 0x03, 0x00, 0xf0, 0x3c,
-      0x60, 0xc6, 0x11, 0x80};
+	const std::vector<BYTE> spsData = {
+		0x64, 0x00, 0x33, 0xac, 0x72, 0x84, 0x40, 0x78,
+		0x02, 0x27, 0xe5, 0xc0, 0x44, 0x00, 0x00, 0x03,
+		0x00, 0x04, 0x00, 0x00, 0x03, 0x00, 0xf0, 0x3c,
+		0x60, 0xc6, 0x11, 0x80 
+	};
 
 
 	H264SeqParameterSet spsParser;
@@ -47,27 +51,27 @@ TEST(TestH26xSPS, TestParseSPS2)
 	// spsParser.Dump();
 
 	EXPECT_EQ(100, spsParser.profile_idc);
-    EXPECT_EQ(0, spsParser.constraint_set0_flag);
-    EXPECT_EQ(0, spsParser.constraint_set1_flag);
-    EXPECT_EQ(0, spsParser.constraint_set2_flag);
-    EXPECT_EQ(0, spsParser.reserved_zero_5bits);
-    EXPECT_EQ(51, spsParser.level_idc);
-    EXPECT_EQ(0, spsParser.seq_parameter_set_id);
-    EXPECT_EQ(1, spsParser.chroma_format_idc);
-    EXPECT_EQ(2, spsParser.log2_max_frame_num_minus4);
-    EXPECT_EQ(0, spsParser.pic_order_cnt_type);
-    EXPECT_EQ(4, spsParser.log2_max_pic_order_cnt_lsb_minus4);
-    EXPECT_EQ(16, spsParser.num_ref_frames);
-    EXPECT_EQ(0, spsParser.gaps_in_frame_num_value_allowed_flag);
-    EXPECT_EQ(119, spsParser.pic_width_in_mbs_minus1);
-    EXPECT_EQ(67, spsParser.pic_height_in_map_units_minus1);
-    EXPECT_EQ(1, spsParser.frame_mbs_only_flag);
-    EXPECT_EQ(1, spsParser.direct_8x8_inference_flag);
-    EXPECT_EQ(1, spsParser.frame_cropping_flag);
-    EXPECT_EQ(0, spsParser.frame_crop_left_offset);
-    EXPECT_EQ(0, spsParser.frame_crop_right_offset);
-    EXPECT_EQ(0, spsParser.frame_crop_top_offset);
-    EXPECT_EQ(4, spsParser.frame_crop_bottom_offset);
+	EXPECT_EQ(0, spsParser.constraint_set0_flag);
+	EXPECT_EQ(0, spsParser.constraint_set1_flag);
+	EXPECT_EQ(0, spsParser.constraint_set2_flag);
+	EXPECT_EQ(0, spsParser.reserved_zero_5bits);
+	EXPECT_EQ(51, spsParser.level_idc);
+	EXPECT_EQ(0, spsParser.seq_parameter_set_id);
+	EXPECT_EQ(1, spsParser.chroma_format_idc);
+	EXPECT_EQ(2, spsParser.log2_max_frame_num_minus4);
+	EXPECT_EQ(0, spsParser.pic_order_cnt_type);
+	EXPECT_EQ(4, spsParser.log2_max_pic_order_cnt_lsb_minus4);
+	EXPECT_EQ(16, spsParser.num_ref_frames);
+	EXPECT_EQ(0, spsParser.gaps_in_frame_num_value_allowed_flag);
+	EXPECT_EQ(119, spsParser.pic_width_in_mbs_minus1);
+	EXPECT_EQ(67, spsParser.pic_height_in_map_units_minus1);
+	EXPECT_EQ(1, spsParser.frame_mbs_only_flag);
+	EXPECT_EQ(1, spsParser.direct_8x8_inference_flag);
+	EXPECT_EQ(1, spsParser.frame_cropping_flag);
+	EXPECT_EQ(0, spsParser.frame_crop_left_offset);
+	EXPECT_EQ(0, spsParser.frame_crop_right_offset);
+	EXPECT_EQ(0, spsParser.frame_crop_top_offset);
+	EXPECT_EQ(4, spsParser.frame_crop_bottom_offset);
 
 
 }
