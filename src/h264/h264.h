1c81c83de7e3b781ff4968c2b22a030da77f1f13
Anand Mahadevan <amaha@dolby.com>
2024-10-24 11:20:00 -0700

    move structs up instead of using a forward declaration

--- src/h264/h264.h
+++ src/h264/h264.h
@@ -44,193 +44,6 @@ static constexpr uint8_t  MaxDpbFrames = 16; // Lifted from FFMPEG
 class H264SeqParameterSet
 {
 public:
-	// forward declarations
-	struct VuiParameters;
-	struct HrdParameters;
-
-	bool Decode(const BYTE* buffer,DWORD bufferSize)
-	{
-		RbspReader reader(buffer, bufferSize);
-		RbspBitReader r(reader);
-
-		try {
-			//Read SPS
-			profile_idc = r.Get(8);
-			constraint_set0_flag = r.Get(1);
-			constraint_set1_flag = r.Get(1);
-			constraint_set2_flag = r.Get(1);
-			reserved_zero_5bits  = r.Get(5);
-			level_idc = r.Get(8);
-			seq_parameter_set_id = r.GetExpGolomb();
-		
-			//Check profile
-			if(profile_idc==100 || profile_idc==110 || profile_idc==122 || profile_idc==244 || profile_idc==44 || 
-				profile_idc==83 || profile_idc==86 || profile_idc==118 || profile_idc==128 || profile_idc==138 || 
-				profile_idc==139 || profile_idc==134 || profile_idc==135)
-			{
-				chroma_format_idc = r.GetExpGolomb();
-
-				if( chroma_format_idc == 3 )
-				{
-					separate_colour_plane_flag = r.Get(1);
-				}
-
-				[[maybe_unused]] auto bit_depth_luma_minus8			= r.GetExpGolomb();
-				[[maybe_unused]] auto bit_depth_chroma_minus8			= r.GetExpGolomb();
-				[[maybe_unused]] auto qpprime_y_zero_transform_bypass_flag	= r.Get(1);
-				[[maybe_unused]] auto seq_scaling_matrix_present_flag		= r.Get(1);
-
-				if( seq_scaling_matrix_present_flag )
-				{	
-					for(uint32_t i = 0; i < (( chroma_format_idc != 3 ) ? 8 : 12 ); i++ )
-					{
-						auto seq_scaling_list_present_flag = r.Get(1);
-						if( seq_scaling_list_present_flag)
-						{
-							//Not supported
-							return false;
-	//						if( i < 6 )
-	//							scaling_list( ScalingList4x4[ i ], 16, UseDefaultScalingMatrix4x4Flag[ i ] )
-	//						else
-	//							scaling_list( ScalingList8x8[ i − 6 ], 64, UseDefaultScalingMatrix8x8Flag[ i − 6 ] )
-						}
-					}
-				}
-			}
-
-			log2_max_frame_num_minus4	= r.GetExpGolomb();
-			pic_order_cnt_type		= r.GetExpGolomb();
-
-			if( pic_order_cnt_type == 0 )
-			{
-				log2_max_pic_order_cnt_lsb_minus4 = r.GetExpGolomb();
-			} else if( pic_order_cnt_type == 1 ) {
-				delta_pic_order_always_zero_flag	= r.Get(1);
-				offset_for_non_ref_pic			= r.GetExpGolomb(); 
-				offset_for_top_to_bottom_field		= r.GetExpGolomb(); 
-				num_ref_frames_in_pic_order_cnt_cycle	= r.GetExpGolomb();
-
-				for( DWORD i = 0; i < num_ref_frames_in_pic_order_cnt_cycle; i++ )
-				{
-					offset_for_ref_frame.assign(i,r.GetExpGolomb());
-				}
-			}
-
-			num_ref_frames				= r.GetExpGolomb();
-
-			gaps_in_frame_num_value_allowed_flag	= r.Get(1);
-			pic_width_in_mbs_minus1			= r.GetExpGolomb();
-			pic_height_in_map_units_minus1		= r.GetExpGolomb();
-			frame_mbs_only_flag			= r.Get(1);
-
-			if (!frame_mbs_only_flag)
-			{
-				mb_adaptive_frame_field_flag	= r.Get(1);
-			}
-
-			direct_8x8_inference_flag		= r.Get(1);
-			frame_cropping_flag	= r.Get(1);
-			
-			if (frame_cropping_flag)
-			{
-				frame_crop_left_offset		= r.GetExpGolomb();
-				frame_crop_right_offset		= r.GetExpGolomb();
-				frame_crop_top_offset		= r.GetExpGolomb();
-				frame_crop_bottom_offset	= r.GetExpGolomb();
-			}
-
-			vui_parameters_present_flag = r.Get(1);
-			if (vui_parameters_present_flag) 
-			{
-				VuiParameters vuiParams;
-				if (!vuiParams.Decode(r))
-					return false;
-				this->vuiParams = vuiParams;
-			}
-		}
-		catch (std::exception &e) 
-		{
-			Warning("-H264SeqParameterSet::Decoder() Exception\n");
-			//Error
-			return false;
-		}
-		//OK
-		return true;
-	}
-
-public:
-	DWORD GetWidth()	{ return ((pic_width_in_mbs_minus1 +1)*16) - frame_crop_right_offset *2 - frame_crop_left_offset *2; }
-	DWORD GetHeight()	{ return ((2 - frame_mbs_only_flag)* (pic_height_in_map_units_minus1 +1) * 16) - frame_crop_bottom_offset*2 - frame_crop_top_offset*2; }
-
-	bool GetSeparateColourPlaneFlag() const { return separate_colour_plane_flag; }
-	bool GetFrameMbsOnlyFlag() const { return frame_mbs_only_flag; }
-	BYTE GetLog2MaxFrameNumMinus4() const { return log2_max_frame_num_minus4; }
-	void Dump() const
-	{
-		Debug("[H264SeqParameterSet \n");
-		Debug("\tprofile_idc=%.2x\n",				profile_idc);
-		Debug("\tconstraint_set0_flag=%u\n",			constraint_set0_flag);
-		Debug("\tconstraint_set1_flag=%u\n",			constraint_set1_flag);
-		Debug("\tconstraint_set2_flag=%u\n",			constraint_set2_flag);
-		Debug("\treserved_zero_5bits =%u\n",			reserved_zero_5bits );
-		Debug("\tlevel_idc=%.2x\n",				level_idc);
-		Debug("\tseq_parameter_set_id=%u\n",			seq_parameter_set_id);
-		Debug("\tlog2_max_frame_num_minus4=%u\n",		log2_max_frame_num_minus4);
-		Debug("\tpic_order_cnt_type=%u\n",			pic_order_cnt_type);
-		Debug("\tlog2_max_pic_order_cnt_lsb_minus4=%u\n",	log2_max_pic_order_cnt_lsb_minus4);
-		Debug("\t delta_pic_order_always_zero_flag=%u\n",	delta_pic_order_always_zero_flag);
-		Debug("\toffset_for_non_ref_pic=%d\n",			offset_for_non_ref_pic);
-		Debug("\toffset_for_top_to_bottom_field=%d\n",		offset_for_top_to_bottom_field);
-		Debug("\tnum_ref_frames_in_pic_order_cnt_cycle=%u\n",	num_ref_frames_in_pic_order_cnt_cycle);
-		Debug("\tnum_ref_frames=%u\n",				num_ref_frames);
-		Debug("\tchroma_format_idc=%u\n",				chroma_format_idc);
-		Debug("\tgaps_in_frame_num_value_allowed_flag=%u\n",	gaps_in_frame_num_value_allowed_flag);
-		Debug("\tpic_width_in_mbs_minus1=%u\n",			pic_width_in_mbs_minus1);
-		Debug("\tpic_height_in_map_units_minus1=%u\n",		pic_height_in_map_units_minus1);
-		Debug("\tframe_mbs_only_flag=%u\n",			frame_mbs_only_flag);
-		Debug("\tmb_adaptive_frame_field_flag=%u\n",		mb_adaptive_frame_field_flag);
-		Debug("\tdirect_8x8_inference_flag=%u\n",		direct_8x8_inference_flag);
-		Debug("\tframe_cropping_flag=%u\n",			frame_cropping_flag);
-		Debug("\tframe_crop_left_offset=%u\n",			frame_crop_left_offset);
-		Debug("\tframe_crop_right_offset=%u\n",			frame_crop_right_offset);
-		Debug("\tframe_crop_top_offset=%u\n",			frame_crop_top_offset);
-		Debug("\tframe_crop_bottom_offset=%u\n",		frame_crop_bottom_offset);
-		Debug("\tseparate_colour_plane_flag=%u\n",		separate_colour_plane_flag);
-		if (vuiParams)
-			vuiParams->Dump();
-		Debug("/]\n");
-	}
-	BYTE			profile_idc = 0;
-	bool			constraint_set0_flag = false;
-	bool			constraint_set1_flag = false;
-	bool			constraint_set2_flag = false;
-	BYTE			reserved_zero_5bits  = 0;
-	BYTE			level_idc = 0;
-	BYTE			seq_parameter_set_id = 0;
-	BYTE			log2_max_frame_num_minus4 = 0;
-	BYTE			pic_order_cnt_type = 0;
-	BYTE			log2_max_pic_order_cnt_lsb_minus4 = 0;
-	bool			delta_pic_order_always_zero_flag = false;
-	int			    offset_for_non_ref_pic = 0;
-	int			    offset_for_top_to_bottom_field = 0;
-	BYTE			num_ref_frames_in_pic_order_cnt_cycle = 0;
-	std::vector<int>	offset_for_ref_frame;
-	DWORD			num_ref_frames = 0;
-	DWORD			chroma_format_idc = 0;
-	bool			gaps_in_frame_num_value_allowed_flag = false;
-	DWORD			pic_width_in_mbs_minus1 = 0;
-	DWORD			pic_height_in_map_units_minus1 = 0;
-	bool			frame_mbs_only_flag = false;
-	bool			mb_adaptive_frame_field_flag = false;
-	bool			direct_8x8_inference_flag = false;
-	bool			frame_cropping_flag = false;
-	DWORD			frame_crop_left_offset = 0;
-	DWORD			frame_crop_right_offset = 0;
-	DWORD			frame_crop_top_offset = 0;
-	DWORD			frame_crop_bottom_offset = 0;
-	bool			vui_parameters_present_flag = false;
-	bool			separate_colour_plane_flag = 0;
-
 	struct HrdParameters
 	{
 		uint32_t              cpb_cnt_minus1 = 0;
@@ -551,9 +364,189 @@ public:
 		}
 	};
 
-	std::optional<VuiParameters>   vuiParams;
+	bool Decode(const BYTE* buffer,DWORD bufferSize)
+	{
+		RbspReader reader(buffer, bufferSize);
+		RbspBitReader r(reader);
 
-	
+		try {
+			//Read SPS
+			profile_idc = r.Get(8);
+			constraint_set0_flag = r.Get(1);
+			constraint_set1_flag = r.Get(1);
+			constraint_set2_flag = r.Get(1);
+			reserved_zero_5bits  = r.Get(5);
+			level_idc = r.Get(8);
+			seq_parameter_set_id = r.GetExpGolomb();
+		
+			//Check profile
+			if(profile_idc==100 || profile_idc==110 || profile_idc==122 || profile_idc==244 || profile_idc==44 || 
+				profile_idc==83 || profile_idc==86 || profile_idc==118 || profile_idc==128 || profile_idc==138 || 
+				profile_idc==139 || profile_idc==134 || profile_idc==135)
+			{
+				chroma_format_idc = r.GetExpGolomb();
+
+				if( chroma_format_idc == 3 )
+				{
+					separate_colour_plane_flag = r.Get(1);
+				}
+
+				[[maybe_unused]] auto bit_depth_luma_minus8			= r.GetExpGolomb();
+				[[maybe_unused]] auto bit_depth_chroma_minus8			= r.GetExpGolomb();
+				[[maybe_unused]] auto qpprime_y_zero_transform_bypass_flag	= r.Get(1);
+				[[maybe_unused]] auto seq_scaling_matrix_present_flag		= r.Get(1);
+
+				if( seq_scaling_matrix_present_flag )
+				{	
+					for(uint32_t i = 0; i < (( chroma_format_idc != 3 ) ? 8 : 12 ); i++ )
+					{
+						auto seq_scaling_list_present_flag = r.Get(1);
+						if( seq_scaling_list_present_flag)
+						{
+							//Not supported
+							return false;
+	//						if( i < 6 )
+	//							scaling_list( ScalingList4x4[ i ], 16, UseDefaultScalingMatrix4x4Flag[ i ] )
+	//						else
+	//							scaling_list( ScalingList8x8[ i − 6 ], 64, UseDefaultScalingMatrix8x8Flag[ i − 6 ] )
+						}
+					}
+				}
+			}
+
+			log2_max_frame_num_minus4	= r.GetExpGolomb();
+			pic_order_cnt_type		= r.GetExpGolomb();
+
+			if( pic_order_cnt_type == 0 )
+			{
+				log2_max_pic_order_cnt_lsb_minus4 = r.GetExpGolomb();
+			} else if( pic_order_cnt_type == 1 ) {
+				delta_pic_order_always_zero_flag	= r.Get(1);
+				offset_for_non_ref_pic			= r.GetExpGolomb(); 
+				offset_for_top_to_bottom_field		= r.GetExpGolomb(); 
+				num_ref_frames_in_pic_order_cnt_cycle	= r.GetExpGolomb();
+
+				for( DWORD i = 0; i < num_ref_frames_in_pic_order_cnt_cycle; i++ )
+				{
+					offset_for_ref_frame.assign(i,r.GetExpGolomb());
+				}
+			}
+
+			num_ref_frames				= r.GetExpGolomb();
+
+			gaps_in_frame_num_value_allowed_flag	= r.Get(1);
+			pic_width_in_mbs_minus1			= r.GetExpGolomb();
+			pic_height_in_map_units_minus1		= r.GetExpGolomb();
+			frame_mbs_only_flag			= r.Get(1);
+
+			if (!frame_mbs_only_flag)
+			{
+				mb_adaptive_frame_field_flag	= r.Get(1);
+			}
+
+			direct_8x8_inference_flag		= r.Get(1);
+			frame_cropping_flag	= r.Get(1);
+			
+			if (frame_cropping_flag)
+			{
+				frame_crop_left_offset		= r.GetExpGolomb();
+				frame_crop_right_offset		= r.GetExpGolomb();
+				frame_crop_top_offset		= r.GetExpGolomb();
+				frame_crop_bottom_offset	= r.GetExpGolomb();
+			}
+
+			vui_parameters_present_flag = r.Get(1);
+			if (vui_parameters_present_flag) 
+			{
+				VuiParameters vuiParams;
+				if (!vuiParams.Decode(r))
+					return false;
+				this->vuiParams = vuiParams;
+			}
+		}
+		catch (std::exception &e) 
+		{
+			Warning("-H264SeqParameterSet::Decoder() Exception\n");
+			//Error
+			return false;
+		}
+		//OK
+		return true;
+	}
+
+public:
+	DWORD GetWidth()	{ return ((pic_width_in_mbs_minus1 +1)*16) - frame_crop_right_offset *2 - frame_crop_left_offset *2; }
+	DWORD GetHeight()	{ return ((2 - frame_mbs_only_flag)* (pic_height_in_map_units_minus1 +1) * 16) - frame_crop_bottom_offset*2 - frame_crop_top_offset*2; }
+
+	bool GetSeparateColourPlaneFlag() const { return separate_colour_plane_flag; }
+	bool GetFrameMbsOnlyFlag() const { return frame_mbs_only_flag; }
+	BYTE GetLog2MaxFrameNumMinus4() const { return log2_max_frame_num_minus4; }
+	void Dump() const
+	{
+		Debug("[H264SeqParameterSet \n");
+		Debug("\tprofile_idc=%.2x\n",				profile_idc);
+		Debug("\tconstraint_set0_flag=%u\n",			constraint_set0_flag);
+		Debug("\tconstraint_set1_flag=%u\n",			constraint_set1_flag);
+		Debug("\tconstraint_set2_flag=%u\n",			constraint_set2_flag);
+		Debug("\treserved_zero_5bits =%u\n",			reserved_zero_5bits );
+		Debug("\tlevel_idc=%.2x\n",				level_idc);
+		Debug("\tseq_parameter_set_id=%u\n",			seq_parameter_set_id);
+		Debug("\tlog2_max_frame_num_minus4=%u\n",		log2_max_frame_num_minus4);
+		Debug("\tpic_order_cnt_type=%u\n",			pic_order_cnt_type);
+		Debug("\tlog2_max_pic_order_cnt_lsb_minus4=%u\n",	log2_max_pic_order_cnt_lsb_minus4);
+		Debug("\t delta_pic_order_always_zero_flag=%u\n",	delta_pic_order_always_zero_flag);
+		Debug("\toffset_for_non_ref_pic=%d\n",			offset_for_non_ref_pic);
+		Debug("\toffset_for_top_to_bottom_field=%d\n",		offset_for_top_to_bottom_field);
+		Debug("\tnum_ref_frames_in_pic_order_cnt_cycle=%u\n",	num_ref_frames_in_pic_order_cnt_cycle);
+		Debug("\tnum_ref_frames=%u\n",				num_ref_frames);
+		Debug("\tchroma_format_idc=%u\n",				chroma_format_idc);
+		Debug("\tgaps_in_frame_num_value_allowed_flag=%u\n",	gaps_in_frame_num_value_allowed_flag);
+		Debug("\tpic_width_in_mbs_minus1=%u\n",			pic_width_in_mbs_minus1);
+		Debug("\tpic_height_in_map_units_minus1=%u\n",		pic_height_in_map_units_minus1);
+		Debug("\tframe_mbs_only_flag=%u\n",			frame_mbs_only_flag);
+		Debug("\tmb_adaptive_frame_field_flag=%u\n",		mb_adaptive_frame_field_flag);
+		Debug("\tdirect_8x8_inference_flag=%u\n",		direct_8x8_inference_flag);
+		Debug("\tframe_cropping_flag=%u\n",			frame_cropping_flag);
+		Debug("\tframe_crop_left_offset=%u\n",			frame_crop_left_offset);
+		Debug("\tframe_crop_right_offset=%u\n",			frame_crop_right_offset);
+		Debug("\tframe_crop_top_offset=%u\n",			frame_crop_top_offset);
+		Debug("\tframe_crop_bottom_offset=%u\n",		frame_crop_bottom_offset);
+		Debug("\tseparate_colour_plane_flag=%u\n",		separate_colour_plane_flag);
+		if (vuiParams)
+			vuiParams->Dump();
+		Debug("/]\n");
+	}
+	BYTE			profile_idc = 0;
+	bool			constraint_set0_flag = false;
+	bool			constraint_set1_flag = false;
+	bool			constraint_set2_flag = false;
+	BYTE			reserved_zero_5bits  = 0;
+	BYTE			level_idc = 0;
+	BYTE			seq_parameter_set_id = 0;
+	BYTE			log2_max_frame_num_minus4 = 0;
+	BYTE			pic_order_cnt_type = 0;
+	BYTE			log2_max_pic_order_cnt_lsb_minus4 = 0;
+	bool			delta_pic_order_always_zero_flag = false;
+	int			    offset_for_non_ref_pic = 0;
+	int			    offset_for_top_to_bottom_field = 0;
+	BYTE			num_ref_frames_in_pic_order_cnt_cycle = 0;
+	std::vector<int>	offset_for_ref_frame;
+	DWORD			num_ref_frames = 0;
+	DWORD			chroma_format_idc = 0;
+	bool			gaps_in_frame_num_value_allowed_flag = false;
+	DWORD			pic_width_in_mbs_minus1 = 0;
+	DWORD			pic_height_in_map_units_minus1 = 0;
+	bool			frame_mbs_only_flag = false;
+	bool			mb_adaptive_frame_field_flag = false;
+	bool			direct_8x8_inference_flag = false;
+	bool			frame_cropping_flag = false;
+	DWORD			frame_crop_left_offset = 0;
+	DWORD			frame_crop_right_offset = 0;
+	DWORD			frame_crop_top_offset = 0;
+	DWORD			frame_crop_bottom_offset = 0;
+	bool			vui_parameters_present_flag = false;
+	bool			separate_colour_plane_flag = 0;
+	std::optional<VuiParameters>   vuiParams;
 };
 
 class H264PictureParameterSet
